422947ae05e8e3470c7bc0bdeb7e83c1a660f300
wogud6792 <wogud6792@naver.com>
2019-07-31 20:03:28 +0900

    baepo12

--- blog/forms.py
+++ blog/forms.py
@@ -4,8 +4,8 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['show_title', 'show_poster' , 'show_date', 
-        'show_time', 'show_place', 'post_author', 'ticket_date',
-        'ticket_price', 'ticket_amount', 'bankname', 'account', 
-        'opendate', 'closedate', 'canceldate', 'show_info_text', 'show_info_image',
+        fields = ['show_title', 
+        # 'show_poster' , 'show_date', 'show_time', 'show_place', 'post_author', 'ticket_date',
+        #'ticket_price', 'ticket_amount', 'bankname', 'account', 
+        #'opendate', 'closedate', 'canceldate', 'show_info_text', 'show_info_image',
         ]
