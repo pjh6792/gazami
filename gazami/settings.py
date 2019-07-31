4d01103a190a334666d2e452de0b2bd26ecbd23e
wogud6792 <wogud6792@naver.com>
2019-07-31 19:34:16 +0900

    baepo2

--- gazami/settings.py
+++ gazami/settings.py
@@ -137,3 +137,5 @@ MEDIA_URL = '/media/'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
